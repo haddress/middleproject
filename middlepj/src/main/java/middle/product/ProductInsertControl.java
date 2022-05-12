a14113c3c94dac7558a51c1f299632f2ab850875
haddress <hyunju9366@gmail.com>
2022-05-13 00:30:31 +0900

    0512 상품페이지

---
+++ middlepj/src/main/java/middle/product/ProductInsertControl.java
@@ -0,0 +1,18 @@
+package middle.product;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import middle.web.Control;
+
+public class ProductInsertControl implements Control {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+}
