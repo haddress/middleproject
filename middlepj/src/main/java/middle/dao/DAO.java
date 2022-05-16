03bbb96f4afbbdfd82e62afffc4d4598de9f0d92
Administrator <Administrator@Master>
2022-05-16 23:39:19 +0900

    20220516

--- middlepj/src/main/java/middle/dao/DAO.java
+++ middlepj/src/main/java/middle/dao/DAO.java
@@ -19,7 +19,7 @@ public class DAO {
 	public Connection getConnect() {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver"); 
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@192.168.0.58:1521:xe", "hr", "hr");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost", "hr", "hr"); //192.168.0.58:1521:xe
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
